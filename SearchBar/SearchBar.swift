1a3a3d8d23f77998934c0db5f6cc73e91d903b59
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2021-09-19 14:31:22 +0300

    Add files via upload

---
+++ SearchBar/SearchBar.swift
@@ -0,0 +1,40 @@
+//
+//  SearchBar.swift
+//  Twitter
+//
+//  Created by khawlah khalid on 18/09/2021.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    
+    @Binding var text : String
+    var body: some View {
+        HStack{
+            
+            TextField("Search ...", text: $text)
+             
+                .padding(8)
+                .padding(.horizontal,24)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+            
+                .overlay(Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                            .padding(.leading,8)
+
+                )
+  
+        }
+        .padding(.horizontal,10)
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        
+    }
+}
